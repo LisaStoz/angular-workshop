1ddeb561233993e1d085f50adbc955dc2f187dae
Jelizaveta Potapova <lisastozdev@gmail.com>
2015-02-09 17:10:25 +0000

    added scope watch event

--- main.js
+++ main.js
@@ -10,13 +10,21 @@ var BasketController = function($scope, BillService){
   };
   $scope.anyProducts = false;
 
-  $scope.addProduct = function(){
-    $scope.product.quantity++;
-    $scope.anyProducts = true;
+  var calculateFigures = function(){
+    $scope.anyProducts = ($scope.product.quantity > 0);
     $scope.total = BillService.calculateTotal($scope.product.quantity);
     $scope.discount = BillService.calculateDiscount($scope.product.quantity);
     $scope.final = BillService.calculateFinal($scope.total, $scope.discount);
+  }
+
+  $scope.addProduct = function(){
+    $scope.product.quantity++;
+    calculateFigures();
   };
+
+  $scope.$watch('product.quantity', function(){
+    calculateFigures();
+  }, true);
 };
 
 BasketController.$inject = ['$scope', 'BillService'];
